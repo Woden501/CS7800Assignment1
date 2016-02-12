c92910e5a8194f1b9335628a4733c0e7b2ae2307
Colby Snedeker <colby_sned@sbcglobal.net>
2016-02-12 14:43:53 -0500

    Now properly loading, indexing, and querying cranfield dataset.

---
+++ src/snedeker/lucene/practice/com/models/EntryDocument.java
@@ -0,0 +1,49 @@
+package snedeker.lucene.practice.com.models;
+
+public class EntryDocument {
+	private String docId;
+	private String title;
+	private String contents;
+	private String author;
+	private String additional;
+
+	public String getAdditional() {
+		return additional;
+	}
+
+	public void setAdditional(String additional) {
+		this.additional = additional;
+	}
+
+	public String getDocId() {
+		return docId;
+	}
+
+	public void setDocId(String docId) {
+		this.docId = docId;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+}
--- src/snedeker/lucene/practice/com/services/IndexerService.java
+++ src/snedeker/lucene/practice/com/services/IndexerService.java
@@ -1,12 +1,14 @@
 package snedeker.lucene.practice.com.services;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
@@ -21,62 +23,106 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
+import snedeker.lucene.practice.com.models.EntryDocument;
+
 public class IndexerService {
-	
+
 	private StandardAnalyzer analyzer;
 	Directory index;
-	
+
 	public IndexerService() {
 		analyzer = new StandardAnalyzer();
 		index = new RAMDirectory();
 	}
-	
+
 	public void run() throws IOException, ParseException {
 		IndexWriterConfig config = new IndexWriterConfig(analyzer);
-		
-		String alice = loadText();
-		
+
+		ArrayList<EntryDocument> texts = loadTexts();
+
 		IndexWriter w = new IndexWriter(index, config);
-		addDoc(w, alice, "123456789");
-		addDoc(w, "Lucene In Action", "193398817");
-		addDoc(w, "Lucene for Dummies", "55320055Z");
-		addDoc(w, "Managing Gigabytes","55063554A");
-		addDoc(w, "The Art of Computer Science", "9900333X");
+		for (EntryDocument entry : texts) {
+			addDoc(w, entry);
+		}
 		w.close();
-		
-		String queryString = "alice";
-		
-		Query q = new QueryParser("title", analyzer).parse(queryString);
-		
+
+		String queryString = "elements";
+
+		Query q = new QueryParser("content", analyzer).parse(queryString);
+
 		int hitsPerPage = 10;
 		IndexReader reader = DirectoryReader.open(index);
 		IndexSearcher searcher = new IndexSearcher(reader);
 		TopDocs docs = searcher.search(q, hitsPerPage);
 		ScoreDoc[] hits = docs.scoreDocs;
-		
-		System.out.println("Found " + hits.length + "hits");
+
+		System.out.println("Found " + hits.length + " hits");
 		for (int i = 0; i < hits.length; ++i) {
 			int docId = hits[i].doc;
 			Document d = searcher.doc(docId);
-			System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+			System.out.println((i + 1) + ". " + d.get("title"));
 		}
 	}
-	
-	private String loadText() {
-		String text = "";
-		try (Scanner scanner = new Scanner(IndexerService.class.getResourceAsStream("/snedeker/lucene/practice/com/resources/AliceInWonderland.txt"), "UTF-8").useDelimiter("\\A"))
-		{
-			text = scanner.next();
+
+	private ArrayList<EntryDocument> loadTexts() throws IOException {
+		ArrayList<EntryDocument> texts = new ArrayList<>();
+
+		File folder = new File(IndexerService.class.getResource("/snedeker/lucene/practice/com/resources").getPath());
+		File[] files = folder.listFiles();
+
+		for (File file : files) {
+			String text = FileUtils.readFileToString(file);
+
+			// Split into docs
+			String[] splitDocs = text.split(".I");
+
+			for (String doc : splitDocs) {
+				EntryDocument newEntry = getEntry(doc);
+
+				if (newEntry != null)
+					texts.add(newEntry);
+			}
 		}
 
-		return text;
+		return texts;
 	}
-	
-	private void addDoc(IndexWriter w, String title, String isbn) throws IOException
-	{
+
+	private EntryDocument getEntry(String doc) {
+		EntryDocument entry = new EntryDocument();
+
+		if (!doc.equals("")) {
+			// Get ID
+			String[] docId = doc.split(".T");
+			entry.setDocId(StringUtils.trim(docId[0]));
+
+			// get title
+			String[] title = docId[1].split(".A");
+			entry.setTitle(StringUtils.trim(title[0]));
+
+			// get author
+			String[] author = title[1].split(".B");
+			entry.setAuthor(StringUtils.trim(author[0]));
+
+			// get additional
+			String[] additional = author[1].split(".W");
+			entry.setAdditional(StringUtils.trim(additional[0]));
+
+			// get contents
+			entry.setContents(StringUtils.trim(additional[1]));
+
+			return entry;
+		}
+
+		return null;
+	}
+
+	private void addDoc(IndexWriter w, EntryDocument entry) throws IOException {
 		Document doc = new Document();
-		doc.add(new TextField("title", title, Field.Store.YES));
-		doc.add(new StringField("isbn", isbn, Field.Store.YES));
+		doc.add(new TextField("docId", entry.getDocId(), Field.Store.YES));
+		doc.add(new TextField("title", entry.getTitle(), Field.Store.YES));
+		doc.add(new TextField("content", entry.getContents(), Field.Store.YES));
+		doc.add(new TextField("author", entry.getAuthor(), Field.Store.YES));
+		doc.add(new TextField("additional", entry.getAdditional(), Field.Store.YES));
 		w.addDocument(doc);
 	}
 }
